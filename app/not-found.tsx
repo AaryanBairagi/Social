164aef448ab186ac78dee21a05f9852e62aa82f3
AaryanBairagi <aaryanbairagi11@gmail.com>
2026-05-29 23:44:50 +0530

    added ss

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,11 +8,11 @@ export default function NotFound() {
         <div className="w-full max-w-md mx-auto p-6 flex flex-col items-center">
             <div className="w-60 h-60 mb-4 relative">
             <Image
-            src="/page-not-found.jpg"
-            alt="Page Not Found Illustration"
-            layout="fill"
-            objectFit="contain"
-            priority
+              src="/page-not-found.jpg"
+              alt="Page Not Found Illustration"
+              layout="fill"
+              objectFit="contain"
+              priority
             />
             </div>
             <h1 className="text-3xl font-bold text-cyan-700 mb-2 text-center drop-shadow">
@@ -23,10 +23,10 @@ export default function NotFound() {
                 Don't worry, let's bring you back to safety!
             </p>
             <Link
-            href="/dashboard"
-            className="bg-cyan-500 hover:bg-cyan-600 hover:shadow-[0_0_10px_#22d3ee] text-white font-semibold rounded-full px-6 py-2 transition shadow"
+              href="/dashboard"
+              className="bg-cyan-500 hover:bg-cyan-600 hover:shadow-[0_0_10px_#22d3ee] text-white font-semibold rounded-full px-6 py-2 transition shadow"
             >
-            Go to Home
+              Go to Home
             </Link>
         </div>
     </div>
